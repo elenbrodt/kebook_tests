6828375f375a73300d7b13385d6ccbf60da1282b
elenbrodt <elenbrodt@gmail.com>
2023-02-16 17:04:53 -0300

    feat: toggle buttons

--- JS-Test/index.js
+++ JS-Test/index.js
@@ -4,7 +4,14 @@ const right = document.getElementById("right");
 
 const input = document.getElementById("slider");
 
+const btn_monthly = document.querySelector(".btn_monthly");
+const btn_annuity = document.querySelector(".btn_annuity");
+
+const monthly = document.querySelector("#monthly");
+const annuity = document.querySelector("#annuity");
+
 var count = 1;
+var toggle = true;
 
 left.onclick = () =>{
     decreaseProfiles();
@@ -35,4 +42,27 @@ slider.onchange = ()=> {
     numero.innerHTML = `${count}`;
 }
 
+btn_monthly.onclick = () => {
+    toggle = !toggle;
+    changeBackground();
+}
+btn_annuity.onclick = () => {
+    toggle = !toggle;
+    changeBackground();
+}
+
+function changeBackground(){
+    if (toggle){
+        btn_monthly.style.background = "linear-gradient(to right,#c86ef9 , #7839dd)";
+        btn_annuity.style.background = "#F1EAF7";
+        annuity.style.display = "none";
+        monthly.style.display = "block";
+    }else{
+        btn_annuity.style.background = "linear-gradient(to right,#c86ef9 , #7839dd)";
+        btn_monthly.style.background = "#F1EAF7";
+        monthly.style.display = "none";
+        annuity.style.display = "block";
+    }
+}
+    
 
