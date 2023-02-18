42da2759b4891134a94ecf987099f57ebff0cfef
elenbrodt <elenbrodt@gmail.com>
2023-02-18 19:12:53 -0300

    style: mobile

--- CSS-Test/index.js
+++ CSS-Test/index.js
@@ -1,7 +1,10 @@
 const searchIcon = document.querySelector(".search-icon");
 const searchInput = document.querySelector(".search-input");
 const searchBox = document.querySelector(".search-box");
-
+const ul = document.querySelector("ul");
+const nav = document.querySelector("nav");
+const checkbox = document.querySelector(".checkbox");
+const modalCategory = document.querySelector(".modal-category");
 var toggle = true;
 
 searchIcon.onclick=()=>{
@@ -13,8 +16,24 @@ searchIcon.onclick=()=>{
     } 
 }
 
+checkbox.onclick = ()=>{
+    if (toggle){
+        modalCategory.style.display = 'block';
+        toggle = !toggle;
+    } 
+    else{
+        modalCategory.style.display = 'none';
+        toggle = !toggle;
+    } 
+}
+
 function openSearchInput (){
     searchInput.style.display = "inline";
+    ul.style.justifyContent = "center";
+    ul.style.gap = "40px";
+    ul.style.width = "600px";
+    nav.style.justifyContent = "center";
+
     toggle = !toggle;
 }
 
